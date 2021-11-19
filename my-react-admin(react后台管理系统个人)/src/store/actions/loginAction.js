490aa176149c849860d2610930c46f719b33876e
xuyunyun <xuyunyun1103@163.com>
2021-11-19 13:08:10 +0800

    first commit cnki

--- my-react-admin(react后台管理系统个人)/src/store/actions/loginAction.js
+++
@@ -1,6 +0,0 @@
-export const USERNAME = "USERNAME";
-export const PASSWORD = "PASSWORD";
-
-// export const initUser =() =>{
-//     type: ''
-// }
--- my-react-admin(react后台管理系统个人)/src/store/index.js
+++
@@ -1,9 +0,0 @@
-import { createStore, applyMiddleware } from 'redux'
-import thunk from 'redux-thunk' //用来处理副作用，比如ajax
-import rootReducer from './reducers/rootReducer'
-
-//创建store   唯一的数据源
-//第二个参数thunk，必须要使用applyMiddleware中间件包裹，否则不生效
-const store = createStore(rootReducer, applyMiddleware(thunk));
-
-export default store;
